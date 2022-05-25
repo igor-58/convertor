ccf0425acb6fa388f1b3282f7d6f2624123f4415
igor-58 <103938045+igor-58@users.noreply.github.com>
2022-05-25 17:27:10 +0300

    update main file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from datetime import datetime
-from convert_1 import get_kurs, get_nominal, respons, valid_code, spros
+from function import get_kurs, get_nominal, respons, valid_code, spros
 
 kod_valute = spros()
 valid_code(kod_valute)
